abca0c21c2bf81283cf409b9ef771419bb14299d
Jeremy Andrews <jeremy@tag1consulting.com>
2017-05-26 10:44:59 +0200

    make it possible to install netgrasp

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python2
+
 from distutils.core import setup
 
-setup(
-    name="Netgrasp",
-    version="0.8",
-    packages=["netgrasp"],
-    license="2-clause BSD",
-    long_description=open("README.txt").read(),
-)
+setup(name="NetGrasp",
+      version="0.8",
+      author="Jeremy Andrews",
+      author_email="jeremy@tag1consulting.com",
+      maintainer="Jeremy Andrews",
+      maintainer_email="jeremy@tag1consulting.com",
+      url="https://github.com/jeremyandrews/netgrasp",
+      packages=["netgrasp", "netgrasp.config", "netgrasp.database", "netgrasp.notify", "netgrasp.test", "netgrasp.utils"],
+      license="2-clause BSD",
+      description="A passive network scanner",
+      long_description=open("README.txt").read(),
+      scripts=["bin/netgraspctl", "bin/netgraspd"],
+     )
