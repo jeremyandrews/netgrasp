b15c5368e4766fbdf05a90ee92c0bc06b0f8d27d
Jeremy Andrews <jeremy@tag1consulting.com>
2017-05-29 08:42:48 +0200

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name="NetGrasp",
-      version="0.8.0dev",
+      version="0.8.0.dev1",
       author="Jeremy Andrews",
       author_email="jeremy@tag1consulting.com",
       maintainer="Jeremy Andrews",
