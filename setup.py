4106aeedfa6eb6b058597b76754d5fd060a32570
Jeremy Andrews <jeremy@tag1consulting.com>
2017-06-24 15:03:00 +0200

    bump version to 0.9 due to major changes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name="netgrasp",
-      version="0.8.4-dev",
+      version="0.9.0-dev",
       author="Jeremy Andrews",
       author_email="jeremy@tag1consulting.com",
       maintainer="Jeremy Andrews",
